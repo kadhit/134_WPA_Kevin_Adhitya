4a9ee7c97ee8f4ebfd14ffe102a1ad7c34d11c30
kadhit <92635388+kadhit@users.noreply.github.com>
2022-07-24 01:21:05 +0700

    vue init

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="bg-slate-100">
+    <HeaderComponent />
+    <HeroComponent />
+    <VisionMission />
+    <ProductComponent />
+    <FooterComponent />
+  </div>
+</template>
+
+<script>
+import HeaderComponent from "./components/HeaderComponent.vue";
+import HeroComponent from "./components/HeroComponent.vue";
+import VisionMission from "./components/VisionMission.vue";
+import ProductComponent from "./components/ProductComponent.vue";
+import FooterComponent from "./components/FooterComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    HeaderComponent,
+    HeroComponent,
+    VisionMission,
+    ProductComponent,
+    FooterComponent,
+  },
+};
+</script>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,99 @@
+<template>
+  <nav
+    id="navbar"
+    :class="navBarColor"
+    class="fixed top-0 left-0 w-screen mx-auto p-4 z-20 ease-in-out"
+  >
+    <div class="container">
+      <!-- Navbar Items -->
+      <div
+        class="flex items-center justify-between md:justify-between lg:justify-around px-6"
+      >
+        <!-- Logo -->
+        <div class="text-5xl font-serif">
+          <h1 class="font-light">hi<span class="font-bold">feey.</span></h1>
+        </div>
+        <div class="flex items-center justify-end space-x-12">
+          <!-- Menu Items -->
+          <div class="hidden md:flex space-x-12 font-sans font-normal">
+            <a href="">About</a>
+            <a href="">Our Products</a>
+            <a href="">Contact</a>
+          </div>
+          <!-- Shop now Button -->
+          <a
+            href=""
+            class="hidden md:inline py-3 px-6 bg-emerald-500 text-white font-semibold rounded-full"
+            >Shop now</a
+          >
+        </div>
+        <button
+          @click="isToggled = !isToggled"
+          class="block relative md:hidden"
+        >
+          <div v-if="!isToggled"><HamburgerIcon /></div>
+          <div v-else><CloseIcon /></div>
+        </button>
+      </div>
+    </div>
+    <div
+      :class="[!isToggled ? '-translate-y-full' : 'translate-y-0', navBarColor]"
+      class="block absolute -z-10 w-screen h-screen top-20 right-0 md:hidden"
+    >
+      <div
+        :class="navBarMobileFont"
+        class="flex flex-col -z-10 pt-8 space-y-8 justify-end items-center"
+      >
+        <a href="">About</a>
+        <a href="">Our Products</a>
+        <a href="">Contact</a>
+        <a
+          href=""
+          class="py-3 px-6 bg-emerald-400 text-white font-semibold rounded-full"
+          >Shop now</a
+        >
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+import HamburgerIcon from "./icons/HamburgerIcon.vue";
+import CloseIcon from "./icons/CloseIcon.vue";
+
+export default {
+  name: "HeaderComponent",
+  components: {
+    HamburgerIcon,
+    CloseIcon,
+  },
+  data() {
+    return {
+      isToggled: false,
+      scrollPosition: null,
+      viewportHeight: null,
+    };
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+    },
+  },
+  computed: {
+    navBarColor() {
+      return this.scrollPosition > this.viewportHeight * 0.9
+        ? "bg-slate-800 text-slate-200 transition-all duration-500 ease-in-out"
+        : "bg-transparent text-slate-800 transition-all duration-500 ease-in-out";
+    },
+    navBarMobileFont() {
+      return this.scrollPosition > this.viewportHeight * 0.9
+        ? ""
+        : "font-semibold";
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.updateScroll);
+    this.viewportHeight = window.innerHeight;
+  },
+};
+</script>
